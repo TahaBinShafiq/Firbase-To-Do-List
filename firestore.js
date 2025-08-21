f4ae2ce9baaed52d3876fd63b272a2a6f9da005b
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-08-21 23:55:50 +0500

    add data to db

--- firestore.js
+++ firestore.js
@@ -1,4 +1,4 @@
-import { collection, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { collection, query, where, onSnapshot  , ref} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 
-export{collection, query, where, onSnapshot}
+export{collection, query, where, onSnapshot , ref}
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-import { collection, query, where, onSnapshot } from "./firestore.js";
+import { db } from "./config.js";
+import { collection, query, where, onSnapshot, ref } from "./firestore.js";
+
+
+
+const todoRef = ref(db, "todos");
+
+document.querySelector("button").addEventListener("click", () => {
+    const task = document.querySelector("input").value;
+    if (task.trim() !== "") {
+        push(todoRef, {
+            task: task,
+            createdAt: Date.now()   // 👈 local timestamp
+        });
+        document.querySelector("input").value = "";
+    }
+});
 
 const q = query(collection(db, "cities"), where("state", "==", "CA"));
 const unsubscribe = onSnapshot(q, (querySnapshot) => {
-  const cities = [];
-  querySnapshot.forEach((doc) => {
-      cities.push(doc.data().name);
-  });
-  console.log("Current cities in CA: ", cities.join(", "));
+    const cities = [];
+    querySnapshot.forEach((doc) => {
+        cities.push(doc.data().name);
+    });
+    console.log("Current cities in CA: ", cities.join(", "));
 });
