2bec1522a9171adaed543c63e4993693f7de418c
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-08-22 23:44:50 +0500

    complete live to do list with firebase

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ mainBtn.addEventListener("click", async () => {
         })
 
         if (alreadyExist === true) {
-            alert("This value already exists ❌");
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "This value already exists",
+                time: 2000,
+                showConfirmButton: false,
+                timerProgressBar: true
+            });
             input.style.border = "2px solid orange";
             input.value = ""
             return;
@@ -48,7 +55,14 @@ mainBtn.addEventListener("click", async () => {
     })
 
     if (alreadyExist === true) {
-        alert("This value already exists ❌");
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "This value already exists",
+            timer: 1500,
+            showConfirmButton: false,
+            timerProgressBar: true
+        });
         input.style.border = "2px solid orange";
         input.value = ""
         return;
@@ -84,19 +98,19 @@ async function showAllTasks() {
                 <div class="actions">
                   <!-- Edit Button -->
                   <button class="icon-btn edit" type="button" data-id="${docSnap.id}" aria-label="Edit task" title="Edit">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                      aria-hidden="true">
-                      <path d="M15.232 5.232a2.5 2.5 0 1 1 3.536 3.536L8.75 18.786 4 20l1.214-4.75 10.018-10.018z" />
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+</svg>
+
                     <span>Edit</span>
                   </button>
         
                   <!-- Delete Button -->
                   <button class="icon-btn delete" type="button" data-id="${docSnap.id}" aria-label="Delete task" title="Delete">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                      aria-hidden="true">
-                      <path d="M3 6h18M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2m-1 0v14a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2V6h10z" />
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
                     <span>Delete</span>
                   </button>
                 </div>
